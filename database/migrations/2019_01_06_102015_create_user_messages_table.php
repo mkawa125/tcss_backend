ea2dbb640d9cee87cc1cd40992716c273a28c13f
mkawa95 <dahabusaidi@gmail.com>
2019-01-06 15:49:03 +0300

    first push

---
+++ database/migrations/2019_01_06_102015_create_user_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('full_names');
+            $table->string('email');
+            $table->string('subject');
+            $table->text('message_body');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_messages');
+    }
+}
