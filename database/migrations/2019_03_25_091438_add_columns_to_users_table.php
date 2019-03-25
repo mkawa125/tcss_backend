fc8d96ec106beffdc371a493578780cff62ab14c
dahabu saidi <dahabusaidi@gmail.com>
2019-03-25 12:22:10 +0300

    added colums to user table

---
+++ database/migrations/2019_03_25_091438_add_columns_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('email')->nullable();
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('surname');
+            $table->string('role');
+            $table->string('phoneNumber')->nullable();
+            $table->boolean('is_changed')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
