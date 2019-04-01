ebe23df57f9e6f173ed51cdba2dd4485ab407a92
dahabu saidi <dahabusaidi@gmail.com>
2019-04-01 11:35:08 +0300

    removed unwanted migrations

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'changeme'),
+    'secret' => env('JWT_SECRET', 'OUUUgkDNQJu6VMspdW3iGLOAGIU29AKV'),
 
     /*
     |--------------------------------------------------------------------------
