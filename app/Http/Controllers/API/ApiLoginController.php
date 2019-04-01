3a351acb41f9c9f4b8c74cde0713d11ad5abf9be
dahabu saidi <dahabusaidi@gmail.com>
2019-04-01 15:48:45 +0300

    added login and register functions

---
+++ app/Http/Controllers/API/ApiLoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Validator;
+use JWTAuth;
+use Tymon\JWTAuth\Facades\JWTFactory;
+use JWTException;
+use App\Models\User;
+class ApiLoginController extends Controller
+{
+    public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+            'password'=> 'required'
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+        $credentials = $request->only('email', 'password');
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+        return response()->json(compact('token'));
+    }
+}
